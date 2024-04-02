b1c1a81a671ce36e3eb7d6abd874ab389a6331a8
ann04ka <annbezee57@gmail.com>
2024-04-02 17:28:16 +0300

    first

---
+++ main.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+def Panorama(path1='pan1.jpg', path2='pan2.jpg'):
+    img1 = cv.imread(path1)
+    img2 = cv.imread(path2)
+    images = [img1, img2]
+    stitcher = cv.Stitcher.create(cv.Stitcher_PANORAMA)
+    status, panorama = stitcher.stitch(images)
+    if status == cv.Stitcher_OK:
+        cv.imwrite('Panorama.jpg', panorama)
+        cv.imshow('Panorama', panorama)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
+    else:
+        print("Error during stitching:", status)
+
+# Panorama()
+
+def FindMatches(path1='photo_1.jpg', path2='photo_2.jpg', k=0.5):
+    img1 = cv.imread(path1, cv.IMREAD_GRAYSCALE)
+    img2 = cv.imread(path2, cv.IMREAD_GRAYSCALE)
+    MIN_MATCH_COUNT = 10
+    sift = cv.SIFT_create()
+    kp1, des1 = sift.detectAndCompute(img1, None)
+    kp2, des2 = sift.detectAndCompute(img2, None)
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des1, des2, k=2)
+    good = []
+    for m, n in matches:
+        if m.distance < k*n.distance:
+            good.append(m)
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RHO, 5.0)
+        matchesMask = mask.ravel().tolist()
+        h, w = img1.shape
+        pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
+        dst = cv.perspectiveTransform(pts, M)
+        img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+    else:
+        print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+        matchesMask = None
+    draw_params = dict(matchColor=(255,255,255), singlePointColor=None, matchesMask=matchesMask, flags=2)
+    img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    plt.imshow(img3, 'gray'), plt.show()
+    
+FindMatches(k=0.5)
